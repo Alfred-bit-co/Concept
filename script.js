a44f485e3b2a0548589b641f38500fd5ed98192c
Alfred <alfredayitou@gmail.com>
2026-06-16 07:15:53 +0000

    Update TEMPLATE_ID format in script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ====== CONFIGURATION EMAILJS ======
 const SERVICE_ID = "service_4oywrlt";
-const TEMPLATE_ID = "6je910l";
+const TEMPLATE_ID = "template_6je910l";
 
 if (typeof emailjs !== 'undefined') {
   emailjs.init("INtZifFaMD80O403k");
